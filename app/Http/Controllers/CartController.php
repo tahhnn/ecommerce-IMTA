d015156cee08ba789b2b9c1cedca12b315fe3523
tahhnn <thanhphuongmao4@gmail.com>
2024-01-29 10:12:55 +0700

    feat: :art: cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    // cart trong admin
+    public function getAllCart(){
+        $cart = Cart::join('products', 'products.id', '=', 'cart.id_product')
+            ->join('users', 'users.id', '=', 'cart.id_user')
+            ->select('cart.*', 'users.name as user_name', 'products.name as product_name')
+            ->get();
+        return view('admin.cart.list',compact('cart'));
+    }
+    public function getCart(){
+        $_SESSION['carts'] = Cart::where('id_user',$_SESSION['id'])->get();
+        $cart = $_SESSION['carts'];
+    }
+    public function deleteCart($id){
+        $cart_delete = Cart::find($id);
+        $cart_delete->delete();
+        return route(redirect(''));
+    }
 }
